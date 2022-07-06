16ac9967c9e0b4b1daedc5c71af767c34e75823f
Михаил Игнатьев <mishkamba1@gmail.com>
2022-07-06 10:59:47 +0900

    team

--- src/settings/base.py
+++ src/settings/base.py
@@ -134,7 +134,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'ru'
+LANGUAGE_CODE = 'ru-Ru'
 
 TIME_ZONE = 'Asia/Yakutsk'
 
